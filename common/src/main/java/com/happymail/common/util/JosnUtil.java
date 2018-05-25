fc771894774be53ec5832a0b20e20b3b129552e0
daxie1 <15926370716@163.com>
2018-05-25 16:49:33 +0800

    jsonUitl

--- common/src/main/java/com/happymail/common/util/JosnUtil.java
+++ common/src/main/java/com/happymail/common/util/JosnUtil.java
@@ -1,6 +1,23 @@
 package com.happymail.common.util;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+
+/**
+ * 针对FastJson的封装
+ * @author yqian
+ *
+ */
 public class JosnUtil
 {
-
+	/**
+	 * 将bean转化为
+	 * @param t
+	 * @param clazz
+	 * @return
+	 */
+	public static<T> String objectToJson(T t,Class<T> clazz)
+	{
+		return JSONObject.toJSONString(t,);
+	}
 }
--- common/src/test/java/common/test/CommTest.java
+++ common/src/test/java/common/test/CommTest.java
@@ -1,6 +1,18 @@
 package common.test;
 
+import java.util.Date;
+
+import org.junit.Test;
+
+import com.alibaba.fastjson.JSON;
+import com.happaymail.common.bean.User;
+
 public class CommTest
 {
-
+	@Test
+	public void jsonTest()
+	{
+		User user=new User(1,"test","123456",new Date());
+		System.out.println(JSON.toJSONString(user));
+	}
 }

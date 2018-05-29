9acaba3a48e47f34a56e6cc4e6da14e6080e1a1a
daxie1 <15926370716@163.com>
2018-05-29 11:58:44 +0800

    spring-seesion better

--- portal/src/main/java/com/happymail/controller/SessionController.java
+++ portal/src/main/java/com/happymail/controller/SessionController.java
@@ -3,6 +3,7 @@ package com.happymail.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,31 +16,30 @@ import com.happymail.common.util.JsonUtil;
 public class SessionController
 {
 	@RequestMapping("/index")
-	public ModelAndView index(HttpServletRequest request)
+	public String index(HttpServletRequest request,Model model)
 	{
-		ModelAndView model=new ModelAndView();
 		Object userJson=request.getSession().getAttribute("user");
 		User user=null;
 		if(userJson!=null)
 		{
 			user=JsonUtil.ToObject(userJson.toString(),User.class);
-			model.addObject("name", user.getUsername());
-			model.addObject("password",user.getPassword());
+//			model.addObject("name", user.getUsername());
+//			model.addObject("password",user.getPassword());
+			model.addAttribute("username", user.getUsername());
+			model.addAttribute("password",user.getPassword());
 		}
-		model.setViewName("sindex");
-		return model;
+		
+		return "sindex";
 	}
 	@RequestMapping("/login")
 	public ModelAndView login(@RequestParam("username")String username,@RequestParam("password")String password,HttpServletRequest request)
 	{
-		ModelAndView model=new ModelAndView();
 		User user=new User();
 		user.setUsername(username);
 		user.setPassword(password);
 		request.getSession().setAttribute("user", JsonUtil.ToJson(user));
-		model.addObject("name", username);
-		model.addObject("password",password);
-		model.setViewName("slogin");
+		ModelAndView model=new ModelAndView("slogin");
+		model.addObject("usertest", user);
 		return model;
 	}
 }

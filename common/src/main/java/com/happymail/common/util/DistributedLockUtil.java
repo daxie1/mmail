ad6459889a8841109ae3eb24ace7955e73c83a05
daxie1 <15926370716@163.com>
2018-05-31 13:41:50 +0800

    redis lock

--- common/src/main/java/com/happymail/common/util/DistributedLockUtil.java
+++ common/src/main/java/com/happymail/common/util/DistributedLockUtil.java
@@ -1,5 +1,9 @@
 package com.happymail.common.util;
 
+import javax.annotation.PreDestroy;
+
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * 使用Redis实现分布式锁的帮助类
  * @author yu
@@ -22,17 +26,17 @@ public class DistributedLockUtil
 		//2.成功获取锁，设置锁的有效期与value的值一致，并返回true
 		if(setResult!=null&&setResult==1)
 		{
-			JedisUtil.expire(lockName, 5);
+			JedisUtil.expire(lockName, 5);//这一步可能会执行，导致死锁
 			return true;
 		}else
 		{
-			//3.设置不成功时获取超时时间，若现在的时间已超过锁的有效期，则一样可以使用，更新锁的有效期
+			//3.设置不成功时获取超时时间，若现在的时间已超过锁的有效期，则一样可以使用，更新锁的有效期，也能防止死锁(即使某些原因导致key永久有效)
 			Long curentTime=System.currentTimeMillis();
 			Long lockTime=Long.parseLong(JedisUtil.get(lockName));
 			if(curentTime>=lockTime)//已经超时，重新上锁
 			{
 				String newLockTime=curentTime+5000+"";//新的有效期
-				if(outTime.equals(JedisUtil.getset(lockName, newLockTime)))//保险期间判断就的有效期是否一致
+				if(StringUtils.equals(outTime,JedisUtil.getset(lockName, newLockTime)))//获取旧的值，锁是否是旧的值，锁是否安全
 				{
 					JedisUtil.expire(lockName,5);
 					return true;
@@ -48,7 +52,9 @@ public class DistributedLockUtil
 	/**
 	 * 释放分布式锁
 	 * @param lockName
+	 * @PreDestroy tomcat shutdown的时候会执行
 	 */
+	@PreDestroy
 	public static void release(String lockName)
 	{
 		JedisUtil.del(lockName);
--- portal/src/main/java/com/happymail/web/common/schedule/ExampleSchedule.java
+++ portal/src/main/java/com/happymail/web/common/schedule/ExampleSchedule.java
@@ -5,6 +5,7 @@ import javax.annotation.Resource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.happymail.common.util.DistributedLockUtil;
 import com.happymail.serviceinter.IUserService;
 
 import lombok.extern.log4j.Log4j2;
@@ -18,12 +19,22 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class ExampleSchedule
 {
+	private static final String LOCK_NAME="LOCK_NAME";
 	@Resource(name="userService")
 	private IUserService userService;
 	@Scheduled(cron="0 0/1 * * * ?")//每分钟执行一次（分钟的整数倍）
 	public void Test()
 	{
-		log.info("Schedult Test OK");
+		if(DistributedLockUtil.lock(LOCK_NAME))
+		{
+			log.info("ThreadName:{} 获得锁", Thread.currentThread().getName());
+			log.info("<!----------------执行相关操作---------------------------->");
+		}else
+		{
+			log.info("ThreadName:{} 没有获得锁", Thread.currentThread().getName());
+		}
+		DistributedLockUtil.release(LOCK_NAME);
+		log.info("ThreadName:{} 释放锁",Thread.currentThread().getName());
 	}
 	public IUserService getUserService()
 	{
